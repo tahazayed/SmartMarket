4cc8349d84d7875e05261296c2c857941a63e41e
taha zayed <tahazayed@gmail.com>
2017-06-21 00:24:11 +0200

    product

--- SmartMarket.Web/Controllers/MobileAPIController.cs
+++ SmartMarket.Web/Controllers/MobileAPIController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessEntities;
+using System;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace SmartMarket.Web.Controllers
 {
@@ -14,6 +16,41 @@ namespace SmartMarket.Web.Controllers
             return db.Categories;
         }
 
+        [HttpGet]
+        [Route("api/MobileAPI/GetProducts", Name = "GetProducts")]
+        public IQueryable<Product> GetProducts(Guid? categoryId, Guid? companyId, string search = "")
+        {
+            IQueryable<Product> lstProducts;
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                lstProducts = db.Products.Where(p => p.ProductName.Contains(search));
+            }
+            else
+            {
+                lstProducts = db.Products;
+            }
+            if (categoryId.HasValue)
+            {
+                lstProducts = lstProducts.Where(p => p.SubCategory.CategoryId == categoryId.Value);
+            }
+            if (companyId.HasValue)
+            {
+                lstProducts = lstProducts.Where(p => p.CompanyId == companyId.Value);
+            }
+            return lstProducts.OrderBy(p => p.ProductName).ThenByDescending(p => p.Rate);
+        }
+        // GET: api/Products1/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(Guid id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
 
         protected override void Dispose(bool disposing)
         {
