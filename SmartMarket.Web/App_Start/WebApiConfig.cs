cc6ae74880864a5618fe37a8bc783fe54bc29d5e
taha zayed <tahazayed@gmail.com>
2017-06-20 23:49:35 +0200

    2017-06-20

--- SmartMarket.Web/App_Start/WebApiConfig.cs
+++ SmartMarket.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace SmartMarket.Web
@@ -16,9 +15,25 @@ namespace SmartMarket.Web
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            // config.Services.Replace(typeof(IHttpControllerSelector), new NamespaceHttpControllerSelector(config));
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            //config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            // WebAPI when dealing with JSON & JavaScript!
+            // Setup json serialization to serialize classes to camel (std. Json format)
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver =
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ SmartMarket.Web/Controllers/MobileAPIController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessEntities;
+using System.Linq;
+using System.Web.Http;
+
+namespace SmartMarket.Web.Controllers
+{
+    public class MobileAPIController : ApiController
+    {
+        private SmartMarketDB db = new SmartMarketDB();
+
+        // GET: api/CategoriesAPI
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
