f94dd88a4e228fa7a5a719bf72155ef4be40ca16
taha zayed <tahazayed@gmail.com>
2017-06-30 21:42:09 +0200

    2017-06-30

---
+++ BusinessEntities/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessEntities
+{
+    public class Cart : CommonBaseBusinessEntity
+    {
+        public long CartId { get; set; }
+
+        [Index]
+        public Guid ProductId { get; set; }
+
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+
+        public int Count { get; set; }
+        public System.DateTime DateCreated { get; set; } = DateTime.Now;
+
+    }
+}
--- BusinessEntities/SmartMarketDB.cs
+++ BusinessEntities/SmartMarketDB.cs
@@ -21,6 +21,7 @@ namespace BusinessEntities
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
 
         public DbSet<Order> Orders { get; set; }
 
